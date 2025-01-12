f361643b2d5c0776898d1f6e648733c714e08c3f
Shashikanth M S <shashikanthms1998@gmail.com>
2025-01-13 00:22:24 +0530

    added main.tf file

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "azurerm" {
+  features {}
+}
+
+variable "resource_group_name" {}
+variable "location" {}
+variable "storage_account_name" {}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
