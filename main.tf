315f6dc11e78827aa39c50529aa9ae468d981919
Shashikanth M S <90105859+shashikanth-m-s@users.noreply.github.com>
2025-02-02 13:52:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,33 @@
 provider "azurerm" {
   features {}
 }
-
+ 
 # Create a resource group
 resource "azurerm_resource_group" "rg" {
-  name     = "myResourceGroup"
+  name     = "sms-rg-tf"
   location = "East US"
 }
-
+ 
 # Create a storage account
 resource "azurerm_storage_account" "storage" {
-  name                     = "smsstorageaccountkanthams"
-  resource_group_name      = azurerm_resource_group.rg.name
+  name                     = "smsstrgtf"
+resource_group_name = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
-
+ 
 # Add a resource lock to the resource group
 resource "azurerm_management_lock" "rg_lock" {
-  name       = "xyz-lock-name"  # Replace with your desired lock name
-  scope      = azurerm_resource_group.rg.id
-  lock_level = "CanNotDelete"   # Options: "CanNotDelete" or "ReadOnly"
+  name       = "sms-lock"  # Lock for the resource group
+scope = azurerm_resource_group.rg.id
+  lock_level = "CanNotDelete"   # Options: "ReadOnly"
 }
+ 
+# Add a resource lock to the storage account
+resource "azurerm_management_lock" "storage_lock" {
+  name       = "sms-lock"  # Lock for the storage account
+scope = azurerm_storage_account.storage.id
+  lock_level = "ReadOnly"       # Options: "CanNotDelete" 
+}
+ 
